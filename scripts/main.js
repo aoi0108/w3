e10dc922505c314e003f19d5d7fae55b7e6f5fb7
Aoi <109831244+aoi0108@users.noreply.github.com>
2023-09-21 12:04:12 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -17,14 +17,13 @@ myButton.onclick = function() {
 
 // Image switcher code
 
-let myImage = document.querySelector('img');
+const myImage = document.querySelector("img");
 
-myImage.onclick = function() {
+myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox-icon.png") {
     myImage.setAttribute("src", "images/firefox2.png");
   } else {
     myImage.setAttribute("src", "images/firefox-icon.png");
-  
-}
+  }
 };
